5399960094154a19aab017ef281ea6d2d713c274
woon <jungwoon3024@naver.com>
2025-06-04 14:05:05 +0900

    [Feat] 장고 공식문서 Part4

--- polls/views.py
+++ polls/views.py
@@ -2,21 +2,66 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . models import Question, Choice
+from django.http import Http404
 
-def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+from django.db.models import F
+from django.urls import reverse
+from django.views import generic
+from django.http import HttpResponseRedirect
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+# def index(request):
+#     # return HttpResponse("Hello, world. You're at the polls index.")
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+# # def detail(request, question_id):
+# #     question = get_object_or_404(Question, pk=question_id)
+# #     return render(request, "polls/detail.html", {"question": question})
 
+# def results(request, question_id):
+#     # question = get_object_or_404(Question, pk=question_id)
+#     # return render(request, "polls/results.html", {"question": question})
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+# def vote(request, question_id):
+#     return HttpResponse(f"You're voting on question {question_id}.")
+
+# 메인 페이지 (질문 목록)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+#질문 상세 페이지      
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = "polls/detail.html"
+  context_object_name = "question"
+# 결과 페이지
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = "polls/results.html"
+  context_object_name = "question"
+# 투표 처리 로직
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message":"You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
